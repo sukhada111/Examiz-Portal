51883c0e80698568e01da815eba93ebd1f093e75
Sukhada Virkar <sukhada9987@gmail.com>
2021-02-27 14:54:00 +0530

    created student side profile and sidebar

---
+++ Student/profile.php
@@ -0,0 +1,65 @@
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<?php
+
+include('header.php');
+include('sidebar.php');
+
+
+?>
+<style>
+    .container{
+        margin-left:200px;
+    }
+ 
+        .card-img{
+            width: 280px;
+            height:94%;
+            margin-right:50%;
+            padding-right:40%;
+            padding-top:20px;
+            padding-left: 20px;
+        }
+        .no-gutters{
+            margin-left: 0px;
+            padding-left:0px;
+        }
+        .card-body{
+            margin-left:30%;
+           
+        }
+        .card-title{
+            padding-right:50px;
+        }
+        .card-text{
+            padding-right:50px;
+        }
+</style>
+<div class="container" align="center">
+    <br>
+<h2>Welcome to Examiz!</h2>
+<br>
+<h3>Your one stop destination for all Exam activities.</h3>
+
+<br><br>
+<h4>Profile details:</h4>
+<div class="card card  shadow mb-4" style="max-width: 540px;">
+  <div class="row no-gutters">
+    <div class="col-md-4">
+      <img src="../resources/img/user2.png" class="card-img" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+          <br>
+        <h5 class="card-title">Sukhada Virkar</h5>
+        <br>
+        <p class="card-text">K J Somaiya College of Engineering</p>
+        <p class="card-text">Year of study: TY</p>
+        <!-- <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p> -->
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+</div>
---
+++ Student/sidebar.php
@@ -0,0 +1,87 @@
+<?php
+include('header.php');
+?>
+<div class="wrapper">
+  <div class="top_navbar">
+    <div class="hamburger">
+       <div class="hamburger__inner">
+         <div class="one"></div>
+         <div class="two"></div>
+         <div class="three"></div>
+       </div>
+    </div>
+    <div class="menu">
+      <div class="logo">
+        Examiz
+      </div>
+      <div class="right_menu">
+        <ul>
+          <li>Logout <i class="fas fa-user"></i>
+            <!-- <div class="profile_dd">
+               <div class="dd_item">Profile</div>
+               <div class="dd_item">Change Password</div>
+               <div class="dd_item">Logout</div>
+            </div> -->
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+    
+  <div class="main_container">
+      <div class="sidebar">
+          <div class="sidebar__inner">
+            <div class="profile">
+              <div class="img">
+                <img src="https://i.imgur.com/Ctwf8HA.png" alt="profile_pic">
+              </div>
+              <div class="profile_info">
+                 <p style="color:white">Welcome</p>
+                 <p class="profile_name">Alex John</p>
+              </div>
+            </div>
+            <ul>
+              <li>
+                <a href="profile.php" class="active">
+                  <span class="icon"><i class="fas fa-id-card"></i></span>
+                  <span class="title">My Profile</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="icon"><i class="fas fa-graduation-cap"></i></span>
+                  <span class="title">Exam Dashboard</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="icon"><i class="fas fa-bell"></i></span>
+                  <span class="title">Subscribed exams</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="icon"><i class="fas fa-chart-pie"></i></span>
+                  <span class="title">Competitive exams</span>
+                </a>
+              </li>
+              <li>
+                <a href="#">
+                  <span class="icon"><i class="fas fa-book"></i></span>
+                  <span class="title">Test Prep</span>
+                </a>
+              </li>
+            </ul>
+          </div>
+      </div>
+      <!-- <div class="container">
+       
+      </div> -->
+      <div id="content-wrapper" class="d-flex flex-column">
+
+  <!-- Main Content -->
+  <div id="content">
+  <!-- </div>
+  
+</div>	 -->
+
